fb42702392f1eca993661c96db07ba264a342aca
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2025-12-10 15:55:32 -0500

    Change download source from PROCESS to INTRO

---
+++ 20-NLB36-L01L01-INTRO.sh
@@ -0,0 +1,3 @@
+# update lab environment
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/LB/INTRO/default.conf
+sudo scp /tmp/default.conf nginx:/etc/nginx/conf.d/
--- 20-NLB36-L01L01-PROCESS.sh
+++
@@ -1,3 +0,0 @@
-# update lab environment
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/LB/PROCESS/default.conf
-sudo scp /tmp/default.conf nginx:/etc/nginx/conf.d/
