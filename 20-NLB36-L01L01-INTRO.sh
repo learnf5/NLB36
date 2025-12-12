06e59fffb938dbb4d61877b59ee9f354a08dc330
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2025-12-12 14:25:17 -0500

    Comment out update commands in INTRO script
    Comment out commands for updating lab environment

--- 20-NLB36-L01L01-INTRO.sh
+++ 20-NLB36-L01L01-INTRO.sh
@@ -1,4 +1,4 @@
 # update lab environment
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/default.conf
-sudo scp /tmp/default.conf nginx:/etc/nginx/conf.d/
-sudo ssh nginx sudo rm /etc/nginx/conf.d/main.conf
+###curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/INTRO/default.conf
+###sudo scp /tmp/default.conf nginx:/etc/nginx/conf.d/
+###sudo ssh nginx sudo rm /etc/nginx/conf.d/main.conf
